0511764564db4db2904039195c2d5c25741d041f
zolza <kinga.koziol6@gmail.com>
2015-01-29 22:35:16 +0100

    cwiczenia z funkcjami z eloquent.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,16 @@ var theNumber = Number(prompt("Pick a number", ""));
 if (!isNaN(theNumber))
   alert("Your number is the square root of " +
         theNumber * theNumber);
+
+var countEven = 0;
+while (countEven<20) {
+	console.log(countEven)
+	countEven +=2;
+}
+//dlaczego ta nie daje undefined w konsoli?
+var countEven = 0;
+while (countEven<20) {
+	countEven +=2;
+	console.log(countEven)
+}
+//dlaczego ta daje undefined jako ostatnią wartość w konsoli?
