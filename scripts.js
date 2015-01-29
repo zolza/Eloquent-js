5536ba9046fc64ef4b50e8bae8d370c348d6c7ff
zolza <kinga.koziol6@gmail.com>
2015-01-29 23:31:33 +0100

    cwiczenia cd.

--- scripts.js
+++ scripts.js
@@ -5,13 +5,32 @@ if (!isNaN(theNumber))
 
 var countEven = 0;
 while (countEven<20) {
-	console.log(countEven)
+	console.log(countEven);
 	countEven +=2;
 }
 //dlaczego ta nie daje undefined w konsoli?
 var countEven = 0;
 while (countEven<20) {
 	countEven +=2;
-	console.log(countEven)
+	console.log(countEven);
 }
 //dlaczego ta daje undefined jako ostatnią wartość w konsoli?
+
+var counter = 0;
+var powers = 1;
+while (counter<13) {
+	counter = counter + 1;
+	powers = powers *2;
+	console.log(powers);
+}
+// i znow ostatnia wartosci jest undefined
+
+var counter = 0;
+var powers = 1;
+while (counter<13) {
+	counter += 1;
+	powers *= 2;
+	console.log(powers);
+}
+
+// i znow ostatnia wartosci jest undefined
