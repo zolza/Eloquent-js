45a2edb2477903212e111b78c3923ecc74ef580f
zolza <kinga.koziol6@gmail.com>
2015-02-17 22:03:20 +0100

    zmienne lokalne i definiowane funkcji

--- scripts_CH3.js
+++ scripts_CH3.js
@@ -40,7 +40,7 @@ for (i=0; i<7; i++)
 var landscape = function() {
   var result = "";
   var flat = function(size) {
-    for (var count = 0; count < size; count++)
+    for (var count = 0; count < size; count++) //var definiuje nam zmienna lokalnie
       result += "_";
   };
   var mountain = function(size) {
@@ -57,3 +57,47 @@ var landscape = function() {
   flat(1);
   return result;
 };
+
+
+var launchMissiles = function(value) {
+  missileSystem.launch("now");
+};
+if (safeMode)
+  launchMissiles = function(value) {/* do nothing */};
+//no dobra, ale czym jest tutaj safeMode? Argumentem? Czymś gdzieś zdefiniowanym?
+//wywolanie launchMissiles: 
+//function (value) {
+// missileSystem.launch("now");
+// }
+//to będzie to samo co:
+function launchMissiles (value) {
+  missileSystem.launch("now");
+};
+if (safeMode)
+function launchMissiles (value) {/* do nothing */
+};
+//wywolanie launchMissiles: 
+//function launchMissiles(value) {
+// missileSystem.launch("now");
+// }
+
+function example() {
+  function a() {} // Okay
+  if (something) {
+    function b() {} // Danger!
+  }
+}
+// to mialabym zdefiniowac funkcje pod zmienna i wtedy byloby ok? nie do konca rozumiem ten fragment książki...
+
+
+function greet(who) {
+  console.log("Hello " + who);
+}
+greet("Harry");
+console.log("Bye");
+//dlaczego zaczyna od wywolania greet("harry")?
+function greet(who) {
+  console.log("Hello " + who);
+}
+console.log("Bye");
+//wywoluje tylko BYE.
